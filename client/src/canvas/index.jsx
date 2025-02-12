739c83d8a5f1e3fd2fa0a8454b5b4cc722cccdd3
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-12 10:08:34 +0200

    feat:adding 3D photo style

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -6,21 +6,22 @@ import Shirt from './Shirt';
 
 const CanvasModel = () => {
 	return (
-		<Canvas
-			shadows
-			camera={{ position: [0, 0, 0], fov: 25 }}
-			gl={{ preserveDrawingBuffer: true }}
-			className='w-full max-w-full h-full transition-all ease-in'>
-			<ambientLight intensity={0.5} />
-			<Environment preset='city' />
-
-			<CameraRig>
-				{/*<Backdrop /> */}
-				<Center>
-					<Shirt />
-				</Center>
-			</CameraRig>
-		</Canvas>
+		<div className=' h-screen'>
+			<Canvas
+				shadows
+				camera={{ position: [0, 0, 0], fov: 25 }}
+				gl={{ preserveDrawingBuffer: true }}
+				className='w-full max-w-full h-full transition-all ease-in'>
+				<ambientLight intensity={0.5} />
+				<Environment preset='city' />
+				<CameraRig>
+					<Backdrop />
+					<Center>
+						<Shirt />
+					</Center>
+				</CameraRig>
+			</Canvas>
+		</div>
 	);
 };
 
